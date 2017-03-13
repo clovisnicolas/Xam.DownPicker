356213ec607f86493c96edcc5cfe1cdda100d65f
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-03-13 17:34:37 +0100

    Create DownPicker Binding & add sample
    Create binding
    Add sample

---
+++ Xam.DownPicker.Sample/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+namespace Xam.DownPicker.Sample
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			//Data
+			NSMutableArray array = new NSMutableArray();
+			array.Add(new NSString("First"));
+			array.Add(new NSString("Second"));
+			array.Add(new NSString("Third"));
+			array.Add(new NSString("Fourth"));
+			array.Add(new NSString("Fifth"));
+			//Add Data to our down picker outlet
+			UIDownPicker picker = new UIDownPicker(array);
+			picker.Frame = this.containerView.Bounds;
+
+			this.containerView.AddSubview(picker);
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
---
+++ Xam.DownPicker/ApiDefinition.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace Xam.DownPicker
+{
+	// @interface DownPicker : UIControl <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
+	[BaseType(typeof(UIControl))]
+	interface DownPicker : IUIPickerViewDelegate, IUIPickerViewDataSource, IUITextFieldDelegate
+	{
+		// @property (nonatomic) NSString * text;
+		[Export("text")]
+		string Text { get; set; }
+
+		// @property (nonatomic) NSInteger selectedIndex;
+		[Export("selectedIndex")]
+		nint SelectedIndex { get; set; }
+
+		// -(id)initWithTextField:(UITextField *)tf;
+		[Export("initWithTextField:")]
+		IntPtr Constructor(UITextField tf);
+
+		// -(id)initWithTextField:(UITextField *)tf withData:(NSMutableArray *)data;
+		[Export("initWithTextField:withData:")]
+		IntPtr Constructor(UITextField tf, NSMutableArray data);
+
+		// -(void)setArrowImage:(UIImage *)image;
+		[Export("setArrowImage:")]
+		void SetArrowImage(UIImage image);
+
+		// -(void)setData:(NSMutableArray *)data;
+		[Export("setData:")]
+		void SetData(NSMutableArray data);
+
+		// -(void)setPlaceholder:(NSString *)str;
+		[Export("setPlaceholder:")]
+		void SetPlaceholder(string str);
+
+		// -(void)setPlaceholderWhileSelecting:(NSString *)str;
+		[Export("setPlaceholderWhileSelecting:")]
+		void SetPlaceholderWhileSelecting(string str);
+
+		// -(void)setToolbarDoneButtonText:(NSString *)str;
+		[Export("setToolbarDoneButtonText:")]
+		void SetToolbarDoneButtonText(string str);
+
+		// -(void)setToolbarCancelButtonText:(NSString *)str;
+		[Export("setToolbarCancelButtonText:")]
+		void SetToolbarCancelButtonText(string str);
+
+		// -(void)setToolbarStyle:(UIBarStyle)style;
+		[Export("setToolbarStyle:")]
+		void SetToolbarStyle(UIBarStyle style);
+
+		// -(void)showArrowImage:(BOOL)b;
+		[Export("showArrowImage:")]
+		void ShowArrowImage(bool b);
+
+		// -(UIPickerView *)getPickerView;
+		[Export("getPickerView")]
+		UIPickerView PickerView { get; }
+
+		// -(UITextField *)getTextField;
+		[Export("getTextField")]
+		UITextField TextField { get; }
+
+		// -(NSString *)getValueAtIndex:(NSInteger)index;
+		[Export("getValueAtIndex:")]
+		string GetValueAtIndex(nint index);
+
+		// -(void)setValueAtIndex:(NSInteger)index;
+		[Export("setValueAtIndex:")]
+		void SetValueAtIndex(nint index);
+	}
+
+	// @interface UIDownPicker : UITextField
+	[BaseType(typeof(UITextField))]
+	interface UIDownPicker
+	{
+		// @property (nonatomic, strong) DownPicker * DownPicker;
+		[Export("DownPicker", ArgumentSemantic.Strong)]
+		DownPicker DownPicker { get; set; }
+
+		// -(id)initWithData:(NSMutableArray *)data;
+		[Export("initWithData:")]
+		IntPtr Constructor(NSMutableArray data);
+	}
+}
